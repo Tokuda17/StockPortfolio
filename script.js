38870278eafd681a125aace8c44047787d351981
Michael Tokuda <michael.tokuda17@gmail.com>
2024-05-12 00:18:14 -0400

    Form submission process is fully complete.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-let stocks = [{ symbol: "apple", shares: 10 }];
+let stocks = JSON.parse(sessionStorage.getItem("stocks")) || [];
 
-document
-  .getElementById("stockForm")
-  .addEventListener("submit", function (event) {
-    console.log("Hello");
-    const stockSymbol = document.getElementById("stockSymbol").value;
-    const numberOfShares = document.getElementById("numberOfShares").value;
-    stocks.push({ symbol: stockSymbol, shares: numberOfShares });
-  });
-
+//Function to display stocks on the HTML page
 function displayStocks() {
-  const stockList = document.getElementById("stockList");
+  //retrieves element on the page
+  const stockList = document.getElementById("stocklist");
   stockList.innerHTML = "";
-
+  //creats a new list element to display the stock.
   stocks.forEach(function (stock) {
     let listItem = document.createElement("li");
     listItem.textContent = `${stock.symbol}: ${stock.shares} shares`;
     stockList.appendChild(listItem);
   });
 }
+
+function saveStocks() {
+  sessionStorage.setItem("stocks", JSON.stringify(stocks));
+}
+
+//Function that handles form submission
+document
+  .getElementById("stockForm")
+  .addEventListener("submit", function (event) {
+    //get value of the stock symbol and price of each share
+    const stockSymbol = document.getElementById("stockSymbol").value;
+    const numberOfShares = document.getElementById("numberOfShares").value;
+
+    //add stock to the array
+    stocks.push({ symbol: stockSymbol, shares: numberOfShares });
+
+    //saves stocks to session storage
+    saveStocks();
+    //Updates the display list
+    displayStocks();
+  });
+
+displayStocks();
